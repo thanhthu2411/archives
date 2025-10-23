833d435cdc91101bd5d2fe477589e549cafaaa41
thanhthu2411 <thu24@byui.edu>
2025-10-23 15:12:53 -0600

    update contact.html

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,66 @@
-
+const feedbackForm = document.querySelector(".contact-form-container");
 const userType = document.getElementById("user-type");
 const readerFields = document.getElementById("reader-fields");
 const authorFields = document.getElementById("author-fields");
+const submitBtn = document.querySelector(".contact-form-container button");
+const message = document.querySelector(".thankyou-message");
+const closeBtn = document.querySelector(".thankyou-message button");
+const overlay = document.querySelector(".overlay");
 
-userType.addEventListener("change", () => {
-  const type = userType.value;
-  if (type === "reader") {
+function setForm () {
+    const type = userType.value;
+    if (type === "reader") {
     readerFields.classList.remove("hidden");
     authorFields.classList.add("hidden");
-  } else if (type === "author") {
+
+    // remove required 
+    authorFields.querySelectorAll("input, textarea").forEach((element) => {
+        element.required = false;
+    });
+    readerFields.querySelectorAll("input, textarea").forEach((element) => {
+        element.required = true;
+    });
+
+    } else if (type === "author") {
     authorFields.classList.remove("hidden");
     readerFields.classList.add("hidden");
-  } else {
-    readerFields.classList.add("hidden");
-    authorFields.classList.add("hidden");
-  }
+
+    readerFields.querySelectorAll("input, textarea").forEach((element) => {
+        element.required = false;
+    });
+    authorFields.querySelectorAll("input, textarea").forEach((element) => {
+        element.required = true;
+    });
+
+    } 
+};
+
+setForm();
+
+userType.addEventListener("change", () => {
+    localStorage.setItem("userType", userType.value);
+    setForm();
+
 });
+
+window.addEventListener("load", () => {
+    userType.value = localStorage.getItem("userType") || "";
+    setForm();
+});
+
+
+feedbackForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    message.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    
+    feedbackForm.querySelectorAll("input, textarea").forEach((el) => {
+        el.value = "";
+    })
+})
+
+closeBtn.addEventListener("click", () => {
+    message.classList.add("hidden");
+    overlay.classList.add("hidden");
+})
+
