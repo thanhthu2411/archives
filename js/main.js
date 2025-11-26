d8b278280a0696368b5b375e92d1c9eec8539b0f
thanhthu2411 <thu24@byui.edu>
2025-11-26 16:31:07 -0700

    css animation

--- js/main.js
+++ js/main.js
@@ -11,6 +11,30 @@ function setRecommendedBooks(item) {
     recommendedBookSection.innerHTML = item.map(book => trendingBookTemplate(book)).join("");
 }
 
+function feedbackAnimation() {
+    const contactContainer = document.querySelector(".contact-us-container");
+    const closeBtn = document.querySelector(".fb-close-btn");
+    let isShown = false;
+    let isClosed = false;
+
+    window.addEventListener("scroll", () => {
+        const scrollTop = document.documentElement.scrollTop;
+        const screenHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+        if (!isShown && !isClosed) {
+                if (scrollTop/screenHeight > 0.5) {
+                contactContainer.classList.add("slide-in");
+                isShown = true;
+            }
+        }
+
+        closeBtn.addEventListener("click", () => {
+            contactContainer.classList.remove("slide-in");
+            isClosed = true;
+        })
+    })
+}
+
 
 
 async function init() {
@@ -21,6 +45,7 @@ async function init() {
     setRecommendedBooks(recommendedBookData);
 
     updateBookUrl();
+    feedbackAnimation();
 }
 
 init();
